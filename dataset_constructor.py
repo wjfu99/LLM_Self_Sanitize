74479f7979268cae7c2e18457d246af557490a09
Wenjie Fu <wjfu99@outlook.com>
2024-11-28 16:25:32 +0800

    prepare dataset

--- dataset_constructor.py
+++ dataset_constructor.py
@@ -14,14 +14,11 @@ random.seed(0)
 class CircuitBreakerDataset(Dataset):
     
     def __init__(self, 
-                tokenizer: transformers.PreTrainedTokenizer, 
-                num_examples,
-                lorra_args,
-                model_name_or_path,
+                tokenizer=None, 
+                num_examples=10000,
                 ):
         super(CircuitBreakerDataset, self).__init__()
 
-        self.model_name_or_path = model_name_or_path.lower()
         self.max_length = 1024
 
         one_shot_template = "{user_tag}{instruction}{assistant_tag}<SEPARATOR>{response}"
@@ -31,139 +28,57 @@ class CircuitBreakerDataset(Dataset):
         sep_token = ""
         switch_select = [0]
         use_refusal_retain = False
-        user_tag, assistant_tag = None, None
-        if 'llama-3' in self.model_name_or_path:
-            print("USING LLAMA TEMPLATE")
-            user_tag="<|start_header_id|>user<|end_header_id|>\n\n"
-            assistant_tag="<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
-            switch_select = [0, 1]
-            use_refusal_retain = True
-        elif 'mistral' in self.model_name_or_path:
-            print("USING MISTRAL TEMPLATE")
-            # fix spacing issue in template
-            tokenizer.chat_template = "{{ bos_token }}{% for message in messages %}{% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}{{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}{% endif %}{% if message['role'] == 'user' %}{{ '[INST] ' + message['content'] + ' [/INST]' }}{% elif message['role'] == 'assistant' %}{{ ' ' + message['content'] + eos_token}}{% else %}{{ raise_exception('Only user and assistant roles are supported!') }}{% endif %}{% endfor %}"
-            user_tag="[INST] "
-            assistant_tag=" [/INST]"
-            sep_token = " "
-        else:
-            raise NotImplementedError(f"Config {self.model_name_or_path} not found")
-        
-        assert user_tag and assistant_tag, "user_tag/assistant_tag not defined"
-
-        self.user_tag = user_tag
-        self.assistant_tag = assistant_tag
         self.sep_token = sep_token
 
         # ======================= Retain ======================= #
         ds = load_dataset("HuggingFaceH4/ultrachat_200k", split="test_sft")
-        orig_s = []
-        for example in ds:
-            messages = example["messages"]
-            if len(messages) < 2: continue
-
-            switch = np.random.choice(switch_select)
-            if switch == 0:
-                formatted_input = tokenizer.apply_chat_template(messages, tokenize=False).replace(tokenizer.bos_token, "")
-            elif switch == 1:
-                formatted_input = one_shot_template.format(
-                    user_tag=user_tag, assistant_tag=assistant_tag,
-                    instruction="", response=messages[1]["content"])
-
-            orig_s.append(formatted_input)
-
-            if len(orig_s) > num_examples:
-                break
-        self.orig_s_retain = orig_s
-        random.shuffle(self.orig_s_retain)
-        print("orig_s_retain[0]", orig_s[0])
-        print("Orig s length:", len(self.orig_s_retain))
+        rt_ds = ds.select_columns("messages")
+        
 
         # ======================= Borderline Retain ======================= #
         # from https://github.com/paul-rottger/exaggerated-safety
         with open(f'data/xstest_v2_completions_gpt4_gpteval.csv', newline='') as f:
             data = [dict(row) for row in csv.DictReader(f)]
             data = [row for row in data if row['final_label'] == "1_full_compliance"]
-        
-        borderline_orig_s = []
-        for i, d in enumerate(data * 50):
-            switch = np.random.choice(switch_select)
-            if switch == 0:
-                formatted_input = one_shot_template.format(
-                    user_tag=user_tag, assistant_tag=assistant_tag,
-                    instruction=d['prompt'], response=d['completion'])
-            elif switch == 1:
-                formatted_input = one_shot_template.format(
-                    user_tag=user_tag, assistant_tag=assistant_tag,
-                    instruction="", response=d['completion'])
-            
-            borderline_orig_s.append(formatted_input)
-
-        self.orig_s_retain += borderline_orig_s
-        random.shuffle(self.orig_s_retain)
-        print("borderline_orig_s[0]", borderline_orig_s[0])
-        print("Orig s length:", len(self.orig_s_retain))
+        messages = []
+        for d in data:
+            messages.append([{
+                "role": "user",
+                "content": d['prompt']
+            }, {
+                "role": "assistant",
+                "content": d['completion']
+            }])
+        brt_ds = datasets.Dataset.from_dict({"messages": messages})
 
         # ======================= Refusal Retain ======================= #
-        if use_refusal_retain:
-            with open("data/circuit_breakers_train.json") as file:
-                dataset = json.load(file)
-
-            random.shuffle(dataset)
-            dataset = dataset[:2000]
-            refusal_retain_orig = []
-            for i, d in tqdm(enumerate(dataset*2)):
-                switch = np.random.choice(switch_select)
-                if switch == 0:
-                    formatted_input = one_shot_template.format(
-                        user_tag=user_tag, assistant_tag=assistant_tag,
-                        instruction=d['prompt'], response=d['llama3_output'])
-                elif switch == 1:
-                    formatted_input = one_shot_template.format(
-                        user_tag=user_tag, assistant_tag=assistant_tag,
-                        instruction="", response=d['llama3_output'])
-                
-                refusal_retain_orig.append(formatted_input)
-
-            self.orig_s_retain += refusal_retain_orig
-            random.shuffle(self.orig_s_retain)
-            print("refusal_orig_s[0]", refusal_retain_orig[0])
-            print("Orig s length:", len(self.orig_s_retain))
-
-        # ======================= Circuit Breaker ======================= #
         with open("data/circuit_breakers_train.json") as file:
             dataset = json.load(file)
-        circuit_breaker_orig = []
 
-        for i, d in tqdm(enumerate(dataset)):
-            cb_output = d['output']
-            switch = np.random.choice(switch_select)
-            if switch == 0:
-                formatted_input = one_shot_template.format(
-                    user_tag=user_tag, assistant_tag=assistant_tag,
-                    instruction=d['prompt'], response=cb_output)
-            elif switch == 1:
-                formatted_input = one_shot_template.format(
-                    user_tag=user_tag, assistant_tag=assistant_tag,
-                    instruction="", response=cb_output)
-            
-            circuit_breaker_orig.append(formatted_input)
+        random.shuffle(dataset)
+        dataset = dataset[:2000]
+        refusal_retain_orig = []
+        messages = []
+        for d in dataset:
+            messages.append([{
+                "role": "user",
+                "content": d['prompt']
+            }, {
+                "role": "assistant",
+                "content": d['llama3_output']
+            }])
+        rrt_ds = datasets.Dataset.from_dict({"messages": messages})
         
-        self.circuit_breaker_orig = circuit_breaker_orig
-        random.shuffle(self.circuit_breaker_orig)
-        print("circuit_breaker_orig[0]", circuit_breaker_orig[0])
-        print("Short circuit length:", len(self.circuit_breaker_orig))
-
-        # ======================= Val ======================= #
-        with open("data/circuit_breakers_val.json") as file:
-            dataset = json.load(file)
-        val_orig = []
-        for i, d in tqdm(enumerate(dataset)):
-            val_orig.append(one_shot_template.format(
-                user_tag=user_tag, assistant_tag=assistant_tag,
-                instruction=d['prompt'], response=d['output']))
-
-        self.val_orig = val_orig
-        self.tokenizer = tokenizer
+        messages = []
+        for d in dataset:
+            messages.append([{
+                "role": "user",
+                "content": d['prompt']
+            }, {
+                "role": "assistant",
+                "content": d['output']
+            }])
+        cbk_ds = datasets.Dataset.from_dict({"messages": messages})
 
     def __len__(self):
         return min(len(self.orig_s_retain), len(self.circuit_breaker_orig))
@@ -202,3 +117,5 @@ class CircuitBreakerDataset(Dataset):
             input_ids_val=tokenized_inputs_val["input_ids"],
             attention_mask_val=tokenized_inputs_val["attention_mask"],
         )
+
+train_dataset = CircuitBreakerDataset()

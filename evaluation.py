3cb65141f7dfff326f307f799bf489b9374cff56
Wenjie Fu <wjfu99@outlook.com>
2025-05-09 21:46:03 +0800

    add evaluate for regular chat

--- evaluation.py
+++ evaluation.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description="Evaluate the output of LLMs")
 parser.add_argument("--output_dir", type=str, default="./privacy_datasets/self_repair_results", help="The output directory for the dataset")
+
 parser.add_argument("--model_name", type=str, default="meta-llama/Llama-2-13b-chat-hf", help="The model name to use")
 args = parser.parse_args()
 
@@ -30,7 +31,14 @@ for key, dataset in output_dataset.items():
     pos_dataset = dataset.filter(lambda x: x['label']==1)  
     neg_dataset = dataset.filter(lambda x: x['label']==0)
     
+    logger.info("==========================Malicious Query Scores==========================")
     rouge_scores, _ = utils.eval_rouge(pos_dataset["response"], pos_dataset["entities"])
     # rouge_scores_hf = utils.eval_rouge_hf(pos_dataset["response"], pos_dataset["entities"])
     bleu_scores = utils.eval_bleu(pos_dataset["response"], pos_dataset["entities"])
-        
+    logger.info(f"bleu: {bleu_scores["bleu"]}, rouge1: {rouge_scores['rouge1']}, rouge2: {rouge_scores['rouge2']}, rougel: {rouge_scores['rougeL']}")
+    
+    logger.info("==========================Regular Query Scores============================")
+    ground_truth = [messages[-1]["content"] for messages in neg_dataset["messages"]]
+    rouge_scores, _ = utils.eval_rouge(neg_dataset["response"], ground_truth)
+    bleu_scores = utils.eval_bleu(neg_dataset["response"], ground_truth)
+    logger.info(f"bleu: {bleu_scores["bleu"]}, rouge1: {rouge_scores['rouge1']}, rouge2: {rouge_scores['rouge2']}, rougel: {rouge_scores['rougeL']}")
